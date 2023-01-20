c9d1a3c52c58c57992cde57dd7f6c2587a5e6a64
John Ricaurte <jjricaurtem@gmail.com>
2023-01-20 12:25:04 -0500

    Added linters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-title = 'pokemon';
-imgParent = "https://openseauserdata.com/files/0d17c23c409d8e5cc60de4ed195dbdb3.jpg"
+  title = 'pokemon';
+  imgParent = "https://openseauserdata.com/files/0d17c23c409d8e5cc60de4ed195dbdb3.jpg"
 }
