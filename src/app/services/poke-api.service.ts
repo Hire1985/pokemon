10562815e47422b9b414e7990fb8bb39ef107796
John Ricaurte <jjricaurtem@gmail.com>
2023-01-20 12:28:53 -0500

    fail test

--- src/app/services/poke-api.service.ts
+++ src/app/services/poke-api.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class PokeApiService {
   constructor(private http: HttpClient) {}
 
-  getPokemons(index: number) {
-    return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${index}`);
-  }
+getPokemons(index: number) {
+return this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${index}`);
+}
 }
